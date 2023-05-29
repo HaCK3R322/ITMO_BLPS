79ee9039fbbcaf62dc811511e17dba46c40363ae
dictator_zx <ivanandrosovv@gmail.com>
2023-05-29 10:21:56 +0300

    Deleting stuff from that brunch

--- src/main/java/com/androsov/itmo_blps_lab1/repositories/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.androsov.itmo_blps_lab1.repositories;
-
-import com.androsov.itmo_blps_lab1.entities.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-}
