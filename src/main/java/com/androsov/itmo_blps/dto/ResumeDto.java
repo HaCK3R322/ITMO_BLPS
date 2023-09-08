c1368ef865621a06a5928cd7d088725f6875ccda
dictator_zx <ivanandrosovv@gmail.com>
2023-09-08 14:37:37 +0300

    Resume Dto fix

--- src/main/java/com/androsov/itmo_blps/dto/ResumeDto.java
+++ src/main/java/com/androsov/itmo_blps/dto/ResumeDto.java
@@ -1,43 +1,82 @@
 package com.androsov.itmo_blps.dto;
 
+import com.androsov.itmo_blps.annotations.NullOrNotBlank;
+import com.androsov.itmo_blps.entities.Image;
+import com.androsov.itmo_blps.entities.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.*;
 import javax.validation.constraints.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class ResumeDto {
     @JsonProperty("id")
     private Long id;
 
-    @JsonProperty("username")
-    private String username;
+    @JsonProperty("userId")
+    private Long userId;
 
-    @Size(min = 5, max = 100, message = "Specialization should be in range of 5-100 symbols")
-    private String specialization;
+    @JsonProperty("imageId")
+    @Null
+    private Long imageId;
 
-    @Size(min = 2, max = 50, message = "Name should be in range of 2-50 symbols")
-    @NotBlank(message = "Name required")
-    @NotNull(message = "Name required")
+    // ================================================================================
+
+    @NotBlank(message = "Name is required")
+    @Length(min = 2, max = 50, message = "Name should be in range of 2-50 characters")
     private String name;
-    @Size(min = 2, max = 50, message = "Surname should be in range of 2-50 symbols")
-    @NotBlank(message = "Name required")
-    @NotNull(message = "Name required")
+
+    @NotBlank(message = "Surname is required")
+    @Length(min = 2, max = 50, message = "Surname should be in range of 2-50 characters")
     private String surname;
 
-    @Min(value = 18, message = "Age should not be less that 18")
-    @Max(value = 150, message = "Age should not be grater that 150")
-    @NotNull(message = "Age required")
-    private Integer age;
+    @NotBlank(message = "Patronymic is required")
+    @Length(min = 2, max = 50, message = "Patronymic should be in range of 2-50 characters")
+    private String patronymic;
 
-    @Size(max = 2000, message = "Studying description should not be longer that 2000 symbols")
-    private String studyingDescription;
-    @Size(max = 2000, message = "Jobs description should not be longer that 2000 symbols")
-    private String jobsDescription;
+    @Past(message = "Date of birth should be in the past")
+    private LocalDate dateOfBirth;
 
-    private Long imageId;
+    @NotBlank(message = "City is required")
+    private String city;
+
+    @NullOrNotBlank(message = "Invalid metro station")
+    private String metroStation;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "\\d{10}", message = "Invalid phone number format")
+    private String phoneNumber;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    // ================================================================================
+
+    @NullOrNotBlank(message = "Desired must be null or must not be blank")
+    private String desiredPosition;
+
+    @DecimalMin(value = "0.0", message = "Salary should not be negative")
+    private Double salary;
+
+    @Pattern(regexp = "^(Full|Part-time|Shift|Rotational|Partial Remote)$", message = "Invalid employment value")
+    private String employment;
+
+    // =================================================================================
+
+    // work experience attached to resume by FK resume_id
+
+    // =================================================================================
+
+    @Pattern(regexp = "^(\\w*;)*$", message = "Invalid skills format. Use semicolons to separate skills.")
+    private String skills;
 }
