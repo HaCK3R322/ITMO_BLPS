6579734d3f04422312d4740f9527140871f0c670
Иван <ivanandrosovv@gmail.com>
2023-09-11 13:36:13 +0300

    BitronixConfiguration

---
+++ src/main/java/com/androsov/itmo_blps/configuration/BitronixConfiguration.java
@@ -0,0 +1,32 @@
+package com.androsov.itmo_blps.configuration;
+
+import bitronix.tm.BitronixTransactionManager;
+import bitronix.tm.TransactionManagerServices;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.transaction.SystemException;
+
+@Configuration
+public class BitronixConfiguration {
+
+    @Bean
+    public bitronix.tm.Configuration transactionManagerServices() {
+        bitronix.tm.Configuration configuration = TransactionManagerServices.getConfiguration();
+        configuration.setServerId("1");
+        return configuration;
+    }
+
+    @Bean(name = "bitronixTransactionManager")
+    public BitronixTransactionManager transactionManager(bitronix.tm.Configuration _c){
+        var trans = TransactionManagerServices.getTransactionManager();
+        try {
+            trans.setTransactionTimeout(60);
+        } catch (SystemException e) {
+            throw new RuntimeException(e);
+        }
+        return trans;
+
+    }
+
+}
